ac2e646b40095b862a1276eeb28d6026af14ca43
Abhijeet Somashila <165672033+abhijeetsomashila1@users.noreply.github.com>
2026-09-21 15:43:20 +0530

    UDP test

--- fg25_gateway_firmware/app.c
+++ fg25_gateway_firmware/app.c
@@ -33,6 +33,11 @@
 #include <stdio.h>
 
 #include "app.h"
+#include "socket.h"
+#include "arpa/inet.h"
+#include <string.h>
+
+
 // -----------------------------------------------------------------------------
 //                              Macros and Typedefs
 // -----------------------------------------------------------------------------
@@ -44,7 +49,8 @@
 // -----------------------------------------------------------------------------
 //                                Global Variables
 // -----------------------------------------------------------------------------
-
+static int udp_sock = -1;
+static sockaddr_in6_t br_addr;
 // -----------------------------------------------------------------------------
 //                                Static Variables
 // -----------------------------------------------------------------------------
@@ -52,6 +58,46 @@
 // -----------------------------------------------------------------------------
 //                          Public Function Definitions
 // -----------------------------------------------------------------------------
+
+static void udp_init(void)
+{
+  udp_sock = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+
+  if (udp_sock < 0) {
+    printf("Failed to create UDP socket\r\n");
+    return;
+  }
+
+  memset(&br_addr, 0, sizeof(br_addr));
+
+  br_addr.sin6_family = AF_INET6;
+  br_addr.sin6_port = htons(5000);
+
+  inet_pton(AF_INET6,
+            "fd12:3456::92fd:9fff:feee:9d54", //Border Router IPv6
+            &br_addr.sin6_addr);
+
+  printf("UDP socket created\r\n");
+}
+
+static void udp_send_test(void)
+{
+  const char *msg = "hello from fg25";
+
+  if (udp_sock < 0) {
+    return;
+  }
+
+  int ret = sendto(udp_sock,
+                   msg,
+                   strlen(msg),
+                   0,
+                   (const struct sockaddr *)&br_addr,
+                   sizeof(br_addr));
+
+  printf("UDP send result = %d\r\n", ret);
+}
+
 /* App task function */
 void app_task(void *args)
 {
@@ -60,10 +106,19 @@ void app_task(void *args)
   // connect to the wisun network
   sl_wisun_app_core_util_connect_and_wait();
 
+   printf("Wi-SUN connected\r\n");
+
+  udp_init();
+
+
   while (1) {
     ///////////////////////////////////////////////////////////////////////////
     // Put your application code here!                                       //
     ///////////////////////////////////////////////////////////////////////////
+    udp_send_test();
+
+    osDelay(10000);
+
     sl_wisun_app_core_util_dispatch_thread();
   }
 }
